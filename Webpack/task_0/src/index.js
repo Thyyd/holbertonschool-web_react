06047f0f67c3159f5cd9ef0ed0c823423c7a2514
Thyyd <roastingblade@gmail.com>
2026-05-06 15:17:54 +0200

    Ajout des fichiers de la task 0 : Création d'un setup Webpack basique.

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery'
+
+$('body').append($('<p>').text('Holberton Dashboard'));
+$('body').append($('<p>').text('Dashboard data for the students'));
+$('body').append($('<p>').text('Copyright - Holberton School'));
