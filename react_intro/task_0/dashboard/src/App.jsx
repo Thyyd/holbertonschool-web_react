f5a08354b9ce75b52d3bbcc9aa7af788f0fd50ac
Thyyd <roastingblade@gmail.com>
2026-05-13 11:09:36 +0200

    Ajout des fichiers de la task 0 : Création d'une application basique avec React

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className='App-header'>
         <img src={HbSLogo} alt="holberton logo" />
-        <h1 style={{color: '#e1003c'}}>School dashboard</h1>
+        <h1>School dashboard</h1>
       </div>
 
       <div className='App-body'>
