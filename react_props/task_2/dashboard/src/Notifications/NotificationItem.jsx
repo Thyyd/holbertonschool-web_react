d050d2a48fa8a91bffe105b19c7b571789224dd4
Thyyd <roastingblade@gmail.com>
2026-05-21 00:20:15 +0200

    Ajout des fichiers de la task 2 : Utilisation de props pour améliorer le composant Notifications.

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.jsx
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,9 +1,9 @@
 function NotificationItem({ type, value, html }) {
   return(
     html ?
-    <li data-notification-type={type}
-      dangerouslySetInnerHTML={{ __html: html }}
-      style={{ color: type === 'default' ? "blue" : "red" }} />:
+      <li data-notification-type={type}
+        dangerouslySetInnerHTML={{ __html: html }}
+        style={{ color: type === 'default' ? "blue" : "red" }} />:
       <li data-notification-type={type} style={{ color: type === 'default' ? "blue" : "red" }}>{ value }</li>
   );
 }
