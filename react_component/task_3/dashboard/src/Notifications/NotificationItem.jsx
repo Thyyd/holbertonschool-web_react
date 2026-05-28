417f002d1da8ce5594e8692d7df44460a0ce3b06
Thyyd <roastingblade@gmail.com>
2026-05-28 19:27:28 +0200

    Ajout des fichiers de la task 3 : Ajout d'un nouevau composant BodySection.

---
+++ react_component/task_3/dashboard/src/Notifications/NotificationItem.jsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+class NotificationItem extends Component {
+  render() {
+    return(
+      this.props.html ?
+        <li data-notification-type={this.props.type}
+          dangerouslySetInnerHTML={{ __html: this.props.html }}
+          style={{ color: this.props.type === 'default' ? "blue" : "red" }}
+          onClick={() => this.props.markAsRead(this.props.id)} />:
+        <li data-notification-type={this.props.type}
+          style={{ color: this.props.type === 'default' ? "blue" : "red" }}
+          onClick={() => this.props.markAsRead(this.props.id)}>{ this.props.value }</li>
+    );
+  }
+}
+
+export default NotificationItem;
