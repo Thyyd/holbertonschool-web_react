432aee7e58ad8e9a1c4e1cf4a9da39c34b4fd708
Thyyd <roastingblade@gmail.com>
2026-06-19 16:03:36 +0200

    MàJ des fichiers de la task 7 : Modification texte labels.

--- react_hooks/task_5/dashboard/src/Login/Login.jsx
+++ react_hooks/task_5/dashboard/src/Login/Login.jsx
@@ -11,13 +11,13 @@ function Login({ logIn }) {
 
       <form className='md:flex md:flex-row md:items-center' onSubmit={handleLoginSubmit} >
         <div className='flex flex-col md:flex-row'>
-          <label className='mt-2 md:mt-0 ml-4' htmlFor="email">Email :</label>
+          <label className='mt-2 md:mt-0 ml-4' htmlFor="email">Email</label>
           <input className='ml-4 w-3/5 md:w-auto border border-gray-400 px-1 rounded'
             type="email" name="email" id="email" onChange={handleChangeEmail} value={email} />
         </div>
 
         <div className='flex flex-col md:flex-row'>
-          <label className='mt-2 md:mt-0 ml-4' htmlFor="password">Password :</label>
+          <label className='mt-2 md:mt-0 ml-4' htmlFor="password">Password</label>
           <input className='ml-4 w-3/5 md:w-auto border border-gray-400 px-1 rounded'
             type="password" name="password" id="password" onChange={handleChangePassword} value={password} />
         </div>
