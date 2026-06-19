a5c3d7c7b4670c014b19989c039209a5834fe5fd
Thyyd <roastingblade@gmail.com>
2026-06-19 20:00:12 +0200

    MàJ des fichiers de la task 4 : Changement des valeur credit de string à int

--- react_state/task_4/dashboard/src/App/App.jsx
+++ react_state/task_4/dashboard/src/App/App.jsx
@@ -13,13 +13,13 @@ import { getLatestNotification } from '../utils/utils';
 const notificationsList = [
   {id: 1, type: 'default', value: 'New course available'},
   {id: 2, type: 'urgent', value: 'New resume available'},
-  {id: 3, type: 'urgent', html: getLatestNotification()}
+  {id: 3, type: 'urgent', html: { __html: getLatestNotification() }}
 ];
 // Déclaration de coursesList
 const coursesList = [
-  { id: 1, name: 'ES6', credit: '60'},
-  { id: 2, name: 'Webpack', credit: '20'},
-  { id: 3, name: 'React', credit: '40'}
+  { id: 1, name: 'ES6', credit: 60},
+  { id: 2, name: 'Webpack', credit: 20},
+  { id: 3, name: 'React', credit: 40}
 ];
 
 class App extends Component {
